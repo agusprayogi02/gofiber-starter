9b768ac51fa72b2ef80b72caffa9b1aa8efc23aa
Agus Prayogi <agus21apy@gmail.com>
2024-08-15 19:38:55 +0700

    [UP] docs on readme

--- entity/user.go
+++ entity/user.go
@@ -16,11 +16,11 @@ const (
 )
 
 type User struct {
-	ID       uint     `gorm:"primaryKey;autoIncrement"`
-	Name     string   `gorm:"type:varchar(200);not null"`
-	Email    string   `gorm:"type:varchar(200);uniqueIndex;not null"`
-	Password string   `gorm:"type:varchar(150);not null"`
-	Role     UserRole `gorm:"type:varchar(10);default:user"` // for sql server only
-	// Role     UserRole `gorm:"type:user_role;default:user"` // for mysql and postgres
+	ID       uint   `gorm:"primaryKey;autoIncrement"`
+	Name     string `gorm:"type:varchar(200);not null"`
+	Email    string `gorm:"type:varchar(200);uniqueIndex;not null"`
+	Password string `gorm:"type:varchar(150);not null"`
+	// Role     UserRole `gorm:"type:varchar(10);default:user"` // for sql server only
+	Role UserRole `gorm:"type:user_role;default:user"` // for mysql and postgres
 	gorm.Model
 }
